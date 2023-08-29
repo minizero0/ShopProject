860d1286a775c63ab9598e856cbeb3aacf017a39
yeongmin <dudals1069@gmail.com>
2023-08-29 21:43:37 +0900

    TestCode

--- src/main/java/com/shopping/minizero0/repository/ItemRepository.java
+++ src/main/java/com/shopping/minizero0/repository/ItemRepository.java
@@ -3,5 +3,9 @@ package com.shopping.minizero0.repository;
 import com.shopping.minizero0.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemNm(String itemNm);
 }
